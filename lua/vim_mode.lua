6a5db99263f5e2fef9fcddc628cbe1a639bdf18d
lijinlei <lijinlei@zhihu.com>
2024-05-13 13:37:18 +0800

    feat: vim_mode

--- lua/vim_mode.lua
+++ lua/vim_mode.lua
@@ -1,28 +1,59 @@
 -- https://github.com/hchunhui/librime-lua/wiki/Scripting
 -- https://github.com/hchunhui/librime-lua/blob/master/sample/rime.lua
+-- https://github.com/hchunhui/librime-lua/issues/294
 
-local function is_win()
-	local path_separator = package.config:sub(1, 1)
-	if path_separator == "\\" then
-		return true
-	end
-	return false
-end
+-- 是否在中文模式的情况下被此程序切换为了英文模式，默认为 false，表示不需要重新切换到中文模式
+local prev_is_cn_mode = false
+-- 是否处于普通模式中
+local in_normal_mode = false
 
+-- 自定义切换 vim mode 的逻辑，
+-- 可以自动切换回中文模式
 local function vim_mode(key, env)
-	print(env)
-	-- 如果当前是 win, 且按下了ctrl+[
-	if key:repr() == "Control+bracketleft" then
-		-- 检测当前是不是英文模式(其实是为了减少在win下那个 中/A 的弹窗次数)
-		local get_ascii_mode = env.engine.context:get_option("ascii_mode")
-		-- 如果不是英文模式, 就切换到英文模式
-		if not get_ascii_mode then
-			env.engine.context:set_option("ascii_mode", true)
+	local vmode = env.engine.context:get_option("vmode")
+
+	-- os.execute("osascript -e 'display notification \"" .. vmode .. '" with title "Title"\'')
+
+	-- 只有在 app_options 中配置了 vmode 才会进入切换模式
+	if vmode then
+		-- 当前是否处于英文模式
+		local is_ascii_mode = env.engine.context:get_option("ascii_mode")
+
+		if in_normal_mode then
+			if is_ascii_mode then
+				-- 105, -- i
+				-- 97, -- a
+				-- 111, -- o
+				-- 99 -- c
+				if key.keycode == 105 or key.keycode == 97 or key.keycode == 111 or key.keycode == 99 then
+					in_normal_mode = false
+
+					-- 如果之前是中文模式，并且将要进入 insert mode，切换到中文模式
+					if prev_is_cn_mode then
+						env.engine.context:set_option("ascii_mode", false)
+						return 1
+					end
+				end
+			end
 		end
-		-- 因为前面的ctrl+[ 被接管了, 所有需要发送一个ctrl+[ 给vim, 使其到normal模式
-		env.engine.commit_text("Control+bracketleft")
-		return 1
+
+		-- 按了 esc 键
+		if key.keycode == 65307 then
+			-- 按了 esc 就认为进入了 normal mode
+			in_normal_mode = true
+
+			if is_ascii_mode then
+				-- 如果当前退出时已经是英文模式，后面插入式就不需要切换为中文模式了
+				prev_is_cn_mode = false
+			else
+				-- 如果当前是中文模式，就需要切换为英文模式
+				-- 并且当重新进入 insert mode 时，需要切换回中文模式
+				prev_is_cn_mode = true
+				env.engine.context:set_option("ascii_mode", true)
+			end
+		end
 	end
+
 	return 2
 end
 
